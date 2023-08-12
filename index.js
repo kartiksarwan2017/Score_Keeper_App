56359b3aa6069f6365f1287bbf17ce7ab761690a
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-08-12 13:05:08 +0530

    Event handling

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ let wicket = 0;
 
 var score = 0;
 
-function clickOne(){
-    alert("1 is clicked");
+function addOne(){
+    score += 1;
+    rootElement.render(<App />);
+    console.log(score);
+}
+
+function addTwo() {
+    score += 2;
+    rootElement.render(<App />);
+    console.log(score);
+}
+
+
+function addScore(num) {
+    score += num;
+    rootElement.render(<App />);
+    console.log(score); 
 }
 
 const App = () => (
@@ -12,12 +27,12 @@ const App = () => (
     <h1>SCORE KEEPER</h1>
     <h2>SCORE: {score}/{wicket}</h2>
     <div>
-         <button onClick={clickOne}>1</button>
-         <button>2</button>
-         <button>3</button>
-         <button>4</button>
-         <button>5</button>
-         <button>6</button>
+         <button onClick = {() => addScore(1)}>1</button>
+         <button onClick = {() => addScore(2)}>2</button>
+         <button onClick= {() => addScore(3)}>3</button>
+         <button onClick= {() => addScore(4)}>4</button>
+         <button onClick= {() => addScore(5)}>5</button>
+         <button onClick= {() => addScore(6)}>6</button>
          <button>wicket</button>
     </div>
     </>
