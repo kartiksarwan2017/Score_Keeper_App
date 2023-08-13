5875a112c9da1427e00ded7f6e944a698d8ec4cc
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-08-13 10:48:46 +0530

    Storing the score ball wise in the array

--- index.js
+++ index.js
@@ -2,53 +2,48 @@
 let wicket = 0;
 
 var score = 0;
-
-function addOne(){
-    score += 1;
-    rootElement.render(<App />);
-    console.log(score);
-}
-
-function addTwo() {
-    score += 2;
-    rootElement.render(<App />);
-    console.log(score);
-}
-
+let ballWiseResult = [];
 
 function addScore(num) {
-
-    if(wicket < 10){
-        score += num;
-        rootElement.render(<App />);
-        console.log(score); 
-    }
+  if (wicket < 10) {
+    ballWiseResult.push(num);
+    score += num;
+    rootElement.render(<App />);
+    console.log(ballWiseResult);
+  }
 }
 
 function addWicket() {
-
-    if(wicket < 10){
-        wicket += 1;
-        rootElement.render(<App />);
-    }
-  
+  if (wicket < 10) {
+    ballWiseResult.push("W");
+    wicket += 1;
+    rootElement.render(<App />);
+    console.log(ballWiseResult);
+  }
 }
 
-const App = () => (
-    <>
-    <h1>SCORE KEEPER</h1>
-    <h2>SCORE: {score}/{wicket}</h2>
+const ScoreButtons = () => (
     <div>
-         <button onClick = {addOne}>1</button>
-         <button onClick = {addTwo}>2</button>
-         <button onClick = {() => addScore(3)}>3</button>
-         <button onClick = {() => addScore(4)}>4</button>
-         <button onClick = {() => addScore(5)}>5</button>
-         <button onClick = {() => addScore(6)}>6</button>
-         <button onClick = {addWicket}>wicket</button>
-    </div>
-    </>
+    <button onClick={() => addScore(0)}>0</button>
+    <button onClick={() => addScore(1)}>1</button>
+    <button onClick={() => addScore(2)}>2</button>
+    <button onClick={() => addScore(3)}>3</button>
+    <button onClick={() => addScore(4)}>4</button>
+    <button onClick={() => addScore(5)}>5</button>
+    <button onClick={() => addScore(6)}>6</button>
+    <button onClick={addWicket}>wicket</button>
+  </div>
 )
 
-const rootElement = ReactDOM.createRoot(document.getElementById('root'));
+const App = () => (
+  <>
+    <h1>SCORE KEEPER</h1>
+    <h2>
+      SCORE: {score}/{wicket}
+    </h2>
+    <ScoreButtons />
+  </>
+);
+
+const rootElement = ReactDOM.createRoot(document.getElementById("root"));
 rootElement.render(<App />);
