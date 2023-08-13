d0c915313b2c3f773fcf91117d7e772d1d60d13d
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-08-13 10:37:01 +0530

    Adding Event Handler for Wicket

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ const App = () => (
     <h1>SCORE KEEPER</h1>
     <h2>SCORE: {score}/{wicket}</h2>
     <div>
-         <button onClick = {() => addScore(1)}>1</button>
-         <button onClick = {() => addScore(2)}>2</button>
+         <button onClick = {addOne}>1</button>
+         <button onClick = {addTwo}>2</button>
          <button onClick = {() => addScore(3)}>3</button>
          <button onClick = {() => addScore(4)}>4</button>
          <button onClick = {() => addScore(5)}>5</button>
